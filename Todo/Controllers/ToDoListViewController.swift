c389ee252fde575d997393bfe21a40a96aeeaa80
alexSoftDeveloper <pm.alex.med@gmail.com>
2024-11-11 11:54:08 +0700

    C12 - add new ViewContoller

--- Todo/Controllers/ToDoListViewController.swift
+++ Todo/Controllers/ToDoListViewController.swift
@@ -12,7 +12,6 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
